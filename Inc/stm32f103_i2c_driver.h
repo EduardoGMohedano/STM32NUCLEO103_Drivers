0e3270472b4bc82ffd8835334e944e56f7e3e9fc
eduardo garcia <egarciamohedano@gmail.com>
2021-03-18 21:39:59 -0600

    chore(i2c) add I2C driver files

---
+++ Inc/stm32f103_i2c_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f103_i2c_driver.h
+ *
+ *  Created on: 18 mar. 2021
+ *      Author: egarc
+ */
+
+#ifndef INC_STM32F103_I2C_DRIVER_H_
+#define INC_STM32F103_I2C_DRIVER_H_
+
+#include "stm32f103.h"
+
+#endif /* INC_STM32F103_I2C_DRIVER_H_ */
---
+++ Src/stm32f103_i2c_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f103_i2c_driver.c
+ *
+ *  Created on: 18 mar. 2021
+ *      Author: egarc
+ */
+
+#include "stm32f103_i2c_driver.h"
